d23e5f14746c06359836622e7cc0b64aafcffc80
Aakash <aakashsittu28@gmail.com>
2024-08-02 18:59:51 +0530

    BFHL

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [jsonInput, setJsonInput] = useState('');
+  const [response, setResponse] = useState(null);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post('http://127.0.0.1:5000/bfhl', JSON.parse(jsonInput));
+      setResponse(res.data);
+    } catch (error) {
+      console.error('Error submitting JSON:', error);
+    }
+  };
+
+  const handleOptionChange = (e) => {
+    const { value, checked } = e.target;
+    if (checked) {
+      setSelectedOptions([...selectedOptions, value]);
+    } else {
+      setSelectedOptions(selectedOptions.filter(option => option !== value));
+    }
+  };
+
+  const renderResponse = () => {
+    if (!response) return null;
+    const filteredResponse = selectedOptions.reduce((acc, option) => {
+      acc[option] = response[option];
+      return acc;
+    }, {});
+    return <pre>{JSON.stringify(filteredResponse, null, 2)}</pre>;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>API Interface</h1>
+      <input
+        type="text"
+        value={jsonInput}
+        onChange={(e) => setJsonInput(e.target.value)}
+        placeholder='Enter JSON'
+      />
+      <button onClick={handleSubmit}>Submit</button>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            value="numbers"
+            onChange={handleOptionChange}
+          />
+          Numbers
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            value="alphabets"
+            onChange={handleOptionChange}
+          />
+          Alphabets
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            value="highest_alphabet"
+            onChange={handleOptionChange}
+          />
+          Highest Alphabet
+        </label>
+      </div>
+      <div>{renderResponse()}</div>
     </div>
   );
-}
+};
 
 export default App;
